aa2cdf31e6342ee5afb69bc3cbd2050392bc86f4
Marxho <marxhop@hotmail.com>
2023-01-18 22:24:09 -0500

    adding functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,23 @@
 // Assignment code here
+function generatePassword() {
+console.log('Button clicked!');
+}
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
+// // Write password to the #password input
+ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+ }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
+
+
+
