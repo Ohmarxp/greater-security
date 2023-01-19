8f4d4cae5affe2fab3774346fd9928f459c01eca
Marxho <marxhop@hotmail.com>
2023-01-18 23:09:46 -0500

    adding prompt string messages

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,29 @@
 // Assignment code here
+
 function generatePassword() {
 console.log('Button clicked!');
+
+var userChoice = window.prompt("Enter length of password:");
+if (!userChoice) {
+  return;
+}
+var userChoice = window.prompt("Enter character type:");
+if (!userChoice) {
+  return;
+}
+var userChoice = window.prompt("Enter amount of characters:");
+
+if (!userChoice) {
+  return;
+}
+ 
 }
 
 
-// // Get references to the #generate element
+// Get references to the #generate element
   var generateBtn = document.querySelector("#generate");
 
-// // Write password to the #password input
+// Write password to the #password input
  function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -16,7 +32,7 @@ console.log('Button clicked!');
 
  }
 
-// // Add event listener to generate button
+// Add event listener to generate button
  generateBtn.addEventListener("click", writePassword);
 
 
